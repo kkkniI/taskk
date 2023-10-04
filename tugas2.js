37ea1fd11a48248ff3dfa8e6cf8bedc5bc1eecef
kkkniI <146954311+kkkniI@users.noreply.github.com>
2023-10-04 20:03:46 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,8 @@
+var firstName = "Mochammad Kani"; 
+var lastName = "Daffa"; 
+var age = 25; 
+var isMarried = false; 
+
+console.log (`${firstName} ${lastName}`)
+console.log (age)
+console.log(isMarried)
