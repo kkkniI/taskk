9ab3e0bed26e354a5d7959b1c276cb9dea34651f
kkkniI <146954311+kkkniI@users.noreply.github.com>
2023-10-17 10:34:02 +0700

    Add files via upload

---
+++ tugas3.1.js
@@ -0,0 +1,15 @@
+//Satria Dewanto Indraloka - 211011401081
+
+const restaurant = {
+    name: "Emado's shawarma",
+    city: "Tangerang Selatan",
+    favoriteDrink: "Lemon Tea",
+    favoriteFood: "Lamb Nasi Mandhi",
+    isVegan: false
+};
+
+console.log("Nama Restoran:", restaurant.name);
+console.log("Kota:", restaurant.city);
+console.log("Minuman Favorit:", restaurant.favoriteDrink);
+console.log("Makanan Favorit:", restaurant.favoriteFood);
+console.log("Restoran Vegan?", restaurant.isVegan);
